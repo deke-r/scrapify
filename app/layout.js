e5992b07b88632b0f4fc45bc19be80a908ef6145
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-09-10 17:48:23 +0530

    new tags added

--- app/layout.js
+++ app/layout.js
@@ -15,27 +15,36 @@ import { usePathname } from 'next/navigation';
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
-
   const hideNavFooter = pathname === "/ad" || pathname === "/thank-you";
 
   return (
     <html lang="en">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-SYLRFVRWPE"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SYLRFVRWPE');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MFQHBHCD');
           `}
         </Script>
+        {/* End Google Tag Manager */}
       </head>
 
       <body className="d-flex flex-column min-vh-100">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MFQHBHCD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Bootstrap />
         <TawkToWidget />
         <WhatsAppButton />
